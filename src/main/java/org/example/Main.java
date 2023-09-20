242e8439ba71a071d36c27b97311f53dae4df0ca
bwaobikeze <bwaobikeze@hotmail.com>
2023-09-19 20:52:11 -0500

    change in approuch

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,6 +6,7 @@ import java.util.Queue;
 class StateOfWorld{
     int [] currentLocation;
     boolean [][] dirtinMatrix;
+    String action;
     StateOfWorld(int [] currentLocation, boolean [][] dirtinMatrix){
         this.currentLocation = currentLocation;
         this.dirtinMatrix = dirtinMatrix;
@@ -13,62 +14,66 @@ class StateOfWorld{
         }
 public class Main {
     public static void main(String[] args) {
-        int num = 3;
-        Queue <StateOfWorld> VistedInGrpah = new LinkedList<>();
-        boolean [][] matrixGrpah = new boolean[num][num];
-        int[][] moves = { {-1, 0}, {1, 0}, {0, -1}, {0, 1} };
-        //setting the intial dirt
-        matrixGrpah[2][0] = true;
-        matrixGrpah[2][1] = true;
-        matrixGrpah[2][2] = true;
+            int num = 3;
+            Queue<StateOfWorld> VisitedInGraph = new LinkedList<>();
+            boolean[][] matrixGraph = new boolean[num][num];
+            int[][] moves = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 
-        int []intialStartOfAgent = {1,1};
+            // Setting the initial dirt
+            matrixGraph[2][0] = true;
+            matrixGraph[2][1] = true;
+            matrixGraph[2][2] = true;
 
-        StateOfWorld startingState = new StateOfWorld(intialStartOfAgent, matrixGrpah);
-        VistedInGrpah.add(startingState);
+            int[] initialStartOfAgent = {1, 1};
 
-        //starting the breadth first search
+            StateOfWorld startingState = new StateOfWorld(initialStartOfAgent, matrixGraph);
+            VisitedInGraph.add(startingState);
 
-        while(!VistedInGrpah.isEmpty()){
-            StateOfWorld tempState = VistedInGrpah.poll();
-            boolean goal = true;
+            boolean[][] visited = new boolean[num][num]; // Track visited states
 
-            for(int i = 0; i< num ;i++){
-                for(int j =0; j< num ; j++){
-                    if(tempState.dirtinMatrix[i][j]){
-                        goal = false;
-                        break;
-                    }
-                }
-            }
-            if(goal){
-                System.out.println("Goal Reached");
-                break;
-            }
-            for(int []move: moves){
-                int newRow = tempState.currentLocation[0] + move[0];
-                int newCol = tempState.currentLocation[1] + move[1];
+            // Starting the breadth-first search
+            while (!VisitedInGraph.isEmpty()) {
+                StateOfWorld tempState = VisitedInGraph.poll();
+                boolean goal = true;
 
-                if(newRow >=0 && newRow < num && newCol >= 0 && newCol < num){
-                    boolean[][] newDirt = new boolean[num][num];
-                    for(int i =0; i < num ; i++){
-                        for(int j=0; j < num ; j++){
-                            newDirt[i][j] = tempState.dirtinMatrix[i][j];
+                for (int i = 0; i < num; i++) {
+                    for (int j = 0; j < num; j++) {
+                        if (tempState.dirtinMatrix[i][j]) {
+                            goal = false;
+                            break;
                         }
                     }
-                    //cleaning dirt
-                    if(newDirt[newRow][newCol]){
-                        newDirt[newRow][newCol] = false;
-                        System.out.println("Cleaned dirt at (" + newRow + ", " + newCol + ")");
+                }
+
+                if (goal) {
+                    System.out.println("Goal Reached");
+                    break;
+                }
+
+                for (int[] move : moves) {
+                    int newRow = tempState.currentLocation[0] + move[0];
+                    int newCol = tempState.currentLocation[1] + move[1];
+
+                    if (newRow >= 0 && newRow < num && newCol >= 0 && newCol < num && !visited[newRow][newCol]) {
+                        boolean[][] newDirt = new boolean[num][num];
+                        for (int i = 0; i < num; i++) {
+                            for (int j = 0; j < num; j++) {
+                                newDirt[i][j] = tempState.dirtinMatrix[i][j];
+                            }
+                        }
+                        // Cleaning dirt
+                        if (newDirt[newRow][newCol]) {
+                            newDirt[newRow][newCol] = false;
+                            System.out.println("Cleaned dirt at (" + newRow + ", " + newCol + ")");
+                        }
+                        // Create a new StateWorld and add it to the queue
+                        int[] newLocation = {newRow, newCol};
+                        StateOfWorld newState = new StateOfWorld(newLocation, newDirt);
+                        VisitedInGraph.add(newState);
+                        visited[newRow][newCol] = true; // Mark as visited
                     }
-                    //Create a new StateWorld and add it to the quewe
-                    int [] newLocation = {newRow, newCol};
-                    StateOfWorld newState = new StateOfWorld(newLocation,newDirt);
-                    VistedInGrpah.add(newState);
                 }
             }
-        }
-
     }
 
 }
